6f7d7db4b4bd4c4c48a0f6ed39139e59d3597a5f
bussi2009 <feresdamak@gmail.com>
2025-06-11 11:38:38 +0200

    fixed bugs

--- client.py
+++ client.py
@@ -4,12 +4,44 @@ import threading
 import socket
 
 HOST = 'localhost'
-PORT = 8888
+PORT = 5500
 
 class paintclient:
-    def __init__ (self, master):
+    def __init__(self, master):
         self.master = master
-        self.master.title("Sketshyverse: Demo")
-        self.canvas = tk.canvas(master, width = 900, height = 600, bg = 'white' )
-        
+        self.master.title("sketchyverse: Demo")
+        self.canvas = tk.Canvas(master, width=800, height=600, bg='white')
+        self.canvas.pack()
 
+        self.canvas.bind('<B1-Motion>', self.draw)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((HOST, PORT))
+
+        threading.Thread(target=self.receive_data, daemon=True).start()
+
+    def draw(self, event):
+        x, y = event.x, event.y
+        self.canvas.create_oval(x, y, x+4, y+4, fill='blue', outline='blue')
+        data = pickle.dumps((x, y))
+        self.sock.send(data)
+
+    def receive_data(self):
+        while True:
+            try:
+                data = self.sock.recv(1024)
+                if not data:
+                    break
+                x, y = pickle.loads(data)
+                self.canvas.create_oval(x, y, x+4, y+4, fill='red', outline='red')
+            except:
+                break
+
+
+
+
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = paintclient(root)
+    root.mainloop()
