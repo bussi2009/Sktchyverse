356ee6397d9526aa4be56d148826c1a8200eb5c0
bussi2009 <feresdamak@gmail.com>
2025-06-11 10:32:56 +0200

    basic tk windows and canvas

--- client.py
+++ client.py
@@ -1,1 +1,15 @@
+import tkinter as tk
+import pickle
+import threading
+import socket
+
+HOST = 'localhost'
+PORT = 8888
+
+class paintclient:
+    def __init__ (self, master):
+        self.master = master
+        self.master.title("Sketshyverse: Demo")
+        self.canvas = tk.canvas(master, width = 900, height = 600, bg = 'white' )
+        
 
